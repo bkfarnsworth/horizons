a372b93f1ad3ac3737dc6f829d66477c019ddd1a
bkfarnsworth <bkfarnsworth@gmail.com>
2021-08-09 14:34:47 -0600

    title change

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Horizons</h1>
+      <h2>Learn more about the world we live in</h2>
     </div>
   );
 }
