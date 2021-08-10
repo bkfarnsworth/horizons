ea13f3af21e9e089fa5aaea62301916ea2c16e10
bkfarnsworth <bkfarnsworth@gmail.com>
2021-08-09 22:34:19 -0600

    Basic done state at end of game

--- src/Game.js
+++ src/Game.js
@@ -2,32 +2,60 @@ import React, { useState } from "react";
 
 export default function Game() {
   let [questionNum, setQuestionNum] = useState(0);
+  let [response, setResponse] = useState();
+  let [score, setScore] = useState(0);
   let questions = [
     {
       question: "What countries border Germany?",
-      answer: "France, Netherlands, etc"
+      answer: "a"
     },
     {
       question: "What countries border Peru?",
-      answer: "Paraguay, Something others maybe?"
+      answer: "b"
+    },
+    {
+      question: "What countries border Brazil?",
+      answer: "c"
     }
   ];
 
+  let question = questions[questionNum];
+
   return (
     <div>
-      <Question
-        question={questions[questionNum]}
-        onSubmit={() => setQuestionNum((prev) => prev + 1)}
-      />
+      <div>
+        Score: {score}/{questions.length}
+      </div>
+      {question ? (
+        <Question
+          question={question}
+          onSubmit={() => {
+            if (response === question.answer) {
+              alert("You got it right!");
+              setScore((prev) => prev + 1);
+            } else {
+              alert("You got it wrong");
+            }
+            setQuestionNum((prev) => prev + 1);
+          }}
+          onResponseChange={(e) => {
+            // console.log(val);
+            setResponse(e.target.value);
+          }}
+        />
+      ) : (
+        <div>All done!</div>
+      )}
     </div>
   );
 }
 
-function Question({ question, onSubmit }) {
+function Question({ question, onSubmit, onResponseChange }) {
   return (
     <div>
       Question: {question.question}
-      Answer: {question.answer}
+      Answer (enter a comma delimited list):{" "}
+      <textarea onChange={onResponseChange} />
       <button onClick={onSubmit}>Submit</button>
     </div>
   );
